96110a75e12600a8bc5e2d7b9808787f6f808ad0
Egbert Felica Wibianto <egbert.fewi@gmail.com>
2024-01-26 22:29:17 +0700

    remove unnecessary imports

--- src/pages/category/Add.test.tsx
+++ src/pages/category/Add.test.tsx
@@ -1,8 +1,6 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import axios from 'axios';
 import Add from './Add';
-import userEvent from '@testing-library/user-event';
 
 jest.mock('axios');
 
